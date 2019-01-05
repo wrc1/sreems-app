2e9454f1c2a499bd35685d7c4c0d2fbc77389ce6
wrc1 <wrcron1@gmail.com>
2019-01-05 15:44:07 +0200

    add packeges

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'; 
 import reducers  from 'StoreRedux/reducers';
 import App from 'components/App/App';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(thunk)
+));
 ReactDOM.render(
     <Provider store={store}>
         <App />
